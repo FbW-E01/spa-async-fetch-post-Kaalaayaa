f47c6044136ce056b0c570865c67b016dd5c07f7
Kaalaayaa <kalaya.chatelain@yahoo.fr>
2021-06-29 17:34:03 +0200

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,71 @@ import "babel-polyfill";
 
 // \/ All of your javascript should go here \/
 
+console.log("hi");
+const form = document.querySelector("#submit-form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const message = document.querySelector("#message");
+const checkbox = document.querySelector("#checkbox");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const user = {
+        name : name.value,
+        email: email.value,
+        password: password.value,
+        message: message.value,
+        checkbox: checkbox.value
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "POST",
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json()
+        })
+        .then(result => {
+            console.log(result);
+            return alert("Form submitted " + result.id)
+        })
+})
+
+ const user = {
+        name : name.value,
+        email: email.value,
+        password: password.value,
+        message: message.value,
+        checkbox: checkbox.value
+    };
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+     const user = {
+        name : name.value,
+        email: email.value,
+        password: password.value,
+        message: message.value,
+        checkbox: checkbox.value
+    };
+
+    getUsers(user);
+    return alert("Form submitted ")
+
+});
+
+const getUsers = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+            method: "POST",
+            body: JSON.stringify(user)
+        });
+    } catch (error) {
+        console.log(error);
+        return alert("An error as occured")
+    }
+}
+
